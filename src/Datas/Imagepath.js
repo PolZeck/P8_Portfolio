92f2e7b373eca10da7cfbfe0eada3880eff53fd7
Paul Ledieu <paul.ledieu@gmail.com>
2024-05-21 09:03:05 +0200

    update .webp images

--- src/Datas/Imagepath.js
+++ src/Datas/Imagepath.js
@@ -1,20 +1,20 @@
 // src/Datas/imagePaths.js
-import KasaImg from '../Images/Kasa-img.png';
-import MVGImg from '../Images/mvg-img.png';
-import BookiImg from '../Images/Booki-img.png';
-import SophieImg from '../Images/Sophie-img.png';
-import Booki1 from '../Images/Booki1.png';
-import Booki2 from '../Images/Booki2.png';
-import Booki3 from '../Images/Booki3.png';
-import Kasa1 from '../Images/Kasa1.png';
-import Kasa2 from '../Images/Kasa2.png';
-import Kasa3 from '../Images/Kasa3.png';
-import MVG1 from '../Images/MVG1.png'
-import MVG2 from '../Images/MVG2.png'
-import MVG3 from '../Images/MVG3.png'
-import Sophie1 from '../Images/Sophie1.png'
-import Sophie2 from '../Images/Sophie2.png'
-import Sophie3 from '../Images/Sophie3.png'
+import KasaImg from '../Images/Kasa-img.webp';
+import MVGImg from '../Images/mvg-img.webp';
+import BookiImg from '../Images/Booki-img.webp';
+import SophieImg from '../Images/Sophie-img.webp';
+import Booki1 from '../Images/Booki1.webp';
+import Booki2 from '../Images/Booki2.webp';
+import Booki3 from '../Images/Booki3.webp';
+import Kasa1 from '../Images/Kasa1.webp';
+import Kasa2 from '../Images/Kasa2.webp';
+import Kasa3 from '../Images/Kasa3.webp';
+import MVG1 from '../Images/MVG1.webp'
+import MVG2 from '../Images/MVG2.webp'
+import MVG3 from '../Images/MVG3.webp'
+import Sophie1 from '../Images/Sophie1.webp'
+import Sophie2 from '../Images/Sophie2.webp'
+import Sophie3 from '../Images/Sophie3.webp'
 
 
 const imagePaths = {
