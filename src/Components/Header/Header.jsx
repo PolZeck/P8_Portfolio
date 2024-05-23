f0534b769f5f08effda689752b84f4f1e2c0bf34
Paul Ledieu <paul.ledieu@gmail.com>
2024-05-23 09:45:50 +0200

    animation description Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,3 @@
-// Header.js
 import React from 'react';
 import './Header.css';
 
@@ -7,7 +6,10 @@ const Header = () => {
     <header className="header">
       <div className="container">
         <h1 className="title">Portfolio</h1>
-        <h2 className="subtitle">Paul Ledieu </h2>
+        <h2 className="subtitle">Paul Ledieu</h2>
+        <p className="description">
+          Développeur web Junior spécialisé en JavaScript (React JS et Node JS)
+        </p>
       </div>
     </header>
   );
