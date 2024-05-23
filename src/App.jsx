5a0f114ef3114ee0fdb6e6067b24a8f8d7d5e6db
Paul Ledieu <paul.ledieu@gmail.com>
2024-05-23 22:09:55 +0200

    Update TypeWriting for less 480px fixed x2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header.jsx';
 import Home from './Pages/Home/Home.jsx';
-import ProjectDetail from './Pages/ProjectDetail/ProjectDetail.jsx';
 
 const App = () => {
   return (
@@ -12,7 +11,6 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/projects/:projectId" element={<ProjectDetail />} />
         </Routes>
       </div>
     </Router>
